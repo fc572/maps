123cc8a4c99a6d3e0244ba610b616572b59c5a36
francesco <francesco.calvino@amido.com>
2016-03-01 21:48:46 +0000

    map project first commit

---
+++ littleJson.js
@@ -0,0 +1,41 @@
+myFunction([
+  {
+	"title":"Acton Town",
+	"lat":"51.5025",
+	"lng":"-0.278126"	
+  },
+  {
+	"title":"Acton Central",
+	"lat":"51.50883531",
+	"lng":"-0.263033174"
+  },
+{
+	"title":"WillesdenJunction",
+	"lat":"51.53032031",
+	"lng":"-0.229378995"
+},
+{
+	"title": "Wimbledon",
+	"lat": "51.422",
+	"lng": "-0.20544"
+},
+{
+	"title": "WimbledonPark",
+	"lat": "51.43391",
+	"lng": "-0.19864"
+},
+{
+	"title": "WoodGreen",
+	"lat": "51.59709",
+	"lng": "-0.10939"
+},
+{
+	"title": "Woodford",
+	"lat": "51.60582",
+	"lng": "0.03328"
+},
+{
+	"title": "WoodsidePark",
+	"lat": "51.61817173",
+	"lng": "-0.185578888"
+}])
